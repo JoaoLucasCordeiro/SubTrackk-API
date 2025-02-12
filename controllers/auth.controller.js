864b99e0295730731c775ba8f02d63fb6cfb80fc
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-02-12 13:22:21 -0300

    feat: Sigin fn in the controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
 export const signUp = async (req, res, next) => {
-    
+
     const session = await mongoose.startSession();
     session.startTransaction();
 
@@ -48,9 +50,37 @@ export const signUp = async (req, res, next) => {
 
 export const signIn = async (req, res, next) => {
     try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            const error = new Error('Invalid password');
+            error.statusCode = 401;
+            throw error;
+        };
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        res.status(200).json({
+            sucess: true,
+            message: 'User logged in successfully',
+            data: {
+                token,
+                user,
+            }
+        });
 
     } catch (error) {
-
+        next(error);
     }
 }
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { signIn, signOut, signUp } from "../controllers/auth.controller";
+import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRoutes = Router();
 
-authRoutes.post('sign-up', signUp);
-authRoutes.post('sign-in', signIn);
-authRoutes.post('sign-out', signOut);
+authRoutes.post('/sign-up', signUp);
+authRoutes.post('/sign-in', signIn);
+authRoutes.post('/sign-out', signOut);
+
 
 export default authRoutes;
