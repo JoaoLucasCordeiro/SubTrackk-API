e3f3f88bea56f8ec31ba61ca5ff125f80c777def
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-02-12 13:31:25 -0300

    feat: add fn's to the routes file

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
+import { getUser, getUsers } from "../controllers/user.controller";
 
 const userRoutes = Router();
 
-userRoutes.get('/', (req, res) => res.send({title: 'GET all users'}))
-userRoutes.get('/:id', (req, res) => res.send({title: 'GET users details'}))
-userRoutes.post('/', (req, res) => res.send({title: 'CREATE new user'}))
-userRoutes.put('/:id', (req, res) => res.send({title: 'UPDATE user '}))
-userRoutes.delete('/:id', (req, res) => res.send({title: 'DELETE user'}))
+userRoutes.get('/', getUsers)
+userRoutes.get('/:id', getUser)
+userRoutes.post('/', (req, res) => res.send({ title: 'CREATE new user' }))
+userRoutes.put('/:id', (req, res) => res.send({ title: 'UPDATE user ' }))
+userRoutes.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }))
 
 export default userRoutes;
