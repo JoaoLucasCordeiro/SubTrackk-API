84b5afbba419a88796c4bbbd8959029a1cd21416
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-02-12 14:10:14 -0300

    feat: add middleware fn to the route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middleware/auth.middleware.js";
 
 const userRoutes = Router();
 
 userRoutes.get('/', getUsers)
-userRoutes.get('/:id', getUser)
+userRoutes.get('/:id', authorize, getUser)
 userRoutes.post('/', (req, res) => res.send({ title: 'CREATE new user' }))
 userRoutes.put('/:id', (req, res) => res.send({ title: 'UPDATE user ' }))
 userRoutes.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }))
