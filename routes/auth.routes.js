7891898d2d5b4c1e3a6eb48ba25caa334b141419
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-02-12 11:44:55 -0300

    feat: all fn are now coming from the controllers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
+import { signIn, signOut, signUp } from "../controllers/auth.controller";
 
 const authRoutes = Router();
 
-authRoutes.post('sign-up', (req, res) => res.send({ title: 'Sign Up' }));
-authRoutes.post('sign-in', (req, res) => res.send({ title: 'Sign In' }));
-authRoutes.post('sign-out', (req, res) => res.send({ title: 'Sign Out' }));
+authRoutes.post('sign-up', signUp);
+authRoutes.post('sign-in', signIn);
+authRoutes.post('sign-out', signOut);
 
 export default authRoutes;
