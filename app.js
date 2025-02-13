ed2e540cb95233bb69653f8729899334adbe79ff
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-02-13 11:45:58 -0300

    feat: config the app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import connectToDataBase from './database/mongodb.js';
 import errorMiddleware from './middleware/error.middleware.js';
 import cookieParser from 'cookie-parser';
 import arcjetMiddleware from './middleware/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 
 const app = express();
@@ -17,9 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(arcjetMiddleware)
 
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/subscriptions', subscriptionRoutes);
+app.use('/api/v1/workflow', workflowRouter);
 
 app.use(errorMiddleware);
 
